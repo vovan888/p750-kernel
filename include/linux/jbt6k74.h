d421c2cf112bf1494f396b74d824e5b88d8e1f15
Vladimir Ananiev <vovan888 at gmail com>
2009-03-07 16:45:44 +0300

    jbt6k74 lcd controller support

---
+++ include/linux/jbt6k74.h
@@ -0,0 +1,11 @@
+#ifndef __JBT6K74_H__
+#define __JBT6K74_H__
+
+#include <linux/spi/spi.h>
+
+struct jbt6k74_platform_data {
+	void (*reset)(int devindex, int level);
+	void (*resuming)(int devindex); /* called when LCM is resumed */
+};
+
+#endif
