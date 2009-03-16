13f57a2e3dbe00d035ea7ffcd0511322841ce6ba
Vladimir Ananiev <vovan888 at gmail com>
2009-03-16 23:34:06 +0300

    [w1][ds2780]: add ds2780 family of devices
    Signed-off-by: Vladimir Ananiev <vovan888 at gmail com>

--- drivers/w1/slaves/w1_ds2780.c
+++ drivers/w1/slaves/w1_ds2780.c
@@ -183,13 +183,13 @@ static struct w1_family_ops w1_ds2780_fops = {
 };
 
 static struct w1_family w1_ds2780_family = {
-	.fid = W1_FAMILY_DS2760,	/* FIXME */
+	.fid = W1_FAMILY_DS2780,
 	.fops = &w1_ds2780_fops,
 };
 
 static int __init w1_ds2780_init(void)
 {
-	printk(KERN_INFO "1-Wire driver for the DS2780 battery monitor "
+	printk(KERN_INFO "1-Wire driver for the DS2780 battery monitor"
 	       " chip  - (c) 2009 Vladimir Ananiev\n");
 	idr_init(&bat_idr);
 	return w1_register_family(&w1_ds2780_family);
